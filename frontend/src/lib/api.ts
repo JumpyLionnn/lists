e4c8abce8122e1fc672fa5d81ab6a24f2fc9c211
JumpyLionnn <jumpylionn1@gmail.com>
2023-02-12 22:40:51 +0200

    Fix optional env PUBLIC_SERVER_URL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -4,7 +4,8 @@ import EventEmitter from "events";
 
 const secure = env.PUBLIC_SERVER_SECURE === "true" ? true : false;
 const protocolSuffix =  (secure ? "s" : "") + "://";
-const baseUrl = (env.PUBLIC_SERVER_URL ?? "") + (env.PUBLIC_SERVER_URL.endsWith("/") ? "" : "/");
+const serverUrl = env.PUBLIC_SERVER_URL ?? "";
+const baseUrl = serverUrl + (serverUrl.endsWith("/") ? "" : "/");
 const httpUrl = "http" + protocolSuffix + baseUrl;
 const socketUrl = "ws" + protocolSuffix + baseUrl;
 
