4b07968b9ac61a304769bf01a085e756e23257d2
JumpyLionnn <jumpylionn1@gmail.com>
2023-02-14 16:15:24 +0200

    Add vercel as a build target

--- frontend/src/lib/platform.ts
+++ frontend/src/lib/platform.ts
@@ -1,6 +1,6 @@
 import { env } from "$env/dynamic/public";
 
-const mobile = env.PUBLIC_MOBILE_BUILD === "true" ? true : false;
+const mobile = env.PUBLIC_PLATFORM === "mobile" ? true : false;
 
 export function isMobile() {
     return mobile;
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,14 +1,20 @@
-import nodeAdapter from '@sveltejs/adapter-node';
-import staticAdapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import nodeAdapter from "@sveltejs/adapter-node";
+import staticAdapter from "@sveltejs/adapter-static";
+import vercelAdapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import dotenv from "dotenv";
+dotenv.config();
 
-
-const isMobileBuild = process.env.PUBLIC_MOBILE_BUILD === "true" ? true : false;
 let adapter;
-if(isMobileBuild){
+if(process.env.PUBLIC_PLATFORM === "mobile"){
 	adapter = staticAdapter({
 		pages: "build/mobile",
 		fallback: "200.html"
+	});
+}
+else if(process.env.PUBLIC_PLATFORM === "vercel"){
+	adapter = vercelAdapter({
+		runtime: "nodejs18.x",		
 	});
 }
 else{
