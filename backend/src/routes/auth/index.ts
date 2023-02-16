4ee50e239ce5ee5cfeae3d58d056b1a28f82eedb
JumpyLionnn <jumpylionn1@gmail.com>
2023-02-16 21:36:48 +0200

    Fix a bug that caused logout to result in an error

--- backend/src/routes/auth/index.ts
+++ backend/src/routes/auth/index.ts
@@ -3,13 +3,14 @@ import { setupSignupRoute } from "./signup";
 import { setupLoginRoute } from "./login";
 import { setupCheckAuthRoute } from "./check-auth";
 import { setupLogoutRoute } from "./logout";
+import { authRequired } from 'middlewares/auth';
 
 export function setupAuthRoutes(){
     const router = Router();
 
     router.post("/signup", setupSignupRoute());
     router.post("/login", setupLoginRoute());
-    router.post("/logout", setupLogoutRoute());
+    router.post("/logout", authRequired(), setupLogoutRoute());
     router.get("/check-auth", setupCheckAuthRoute());
 
     return router;
